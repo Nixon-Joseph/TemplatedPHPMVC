00cf63871606068d3221a8cb0170d8fcb3da96bb
Joseph Nieminen Nixon <nieminen432@gmail.com>
2019-10-12 08:46:52 -0500

    Fixed displaying of the page

--- Core/Classes/controller.php
+++ Core/Classes/controller.php
@@ -31,49 +31,49 @@ abstract class Controller {
         $page->Site = OpenFile("./App/Views/Shared/_layout.dat");
         $page->Content = OpenFile("./App/Views/Home/$page->Template.dat");
 
-        $Page->SiteVars["SiteTitle"] = Constants::GET_SITE_NAME();
-        $Page->SiteVars["SiteName"] = Constants::GET_SITE_NAME();
-        $Page->SiteVars["Scripts"] = "";
-        $Page->SiteVars["SiteSubtitle"] = Constants::GET_SITE_SUBTITLE();
-        $Page->SiteVars["SiteDescription"] =Constants::GET_SITE_DESCRIPTION();
+        $page->SiteVars["SiteTitle"] = Constants::GET_SITE_NAME();
+        $page->SiteVars["SiteName"] = Constants::GET_SITE_NAME();
+        $page->SiteVars["Scripts"] = "";
+        $page->SiteVars["SiteSubtitle"] = Constants::GET_SITE_SUBTITLE();
+        $page->SiteVars["SiteDescription"] =Constants::GET_SITE_DESCRIPTION();
 
         //Setup the optional site variables
         if (VIEW_DATA != null && count(VIEW_DATA) > 0) {
-            $Page->SiteVars = array_merge($Page->SiteVars, VIEW_DATA);
+            $page->SiteVars = array_merge($page->SiteVars, VIEW_DATA);
         }
         //Load page specifics
         //Add in the scripts
-        if (isset($Page->Scripts) === true && count($Page->Scripts) > 0) {
-            foreach ($Page->Scripts as $key => $script) {
-                if (strlen($Page->SiteVars["Scripts"]) > 0) {
-                    $Page->SiteVars["Scripts"] .= "\n";
+        if (isset($page->Scripts) === true && count($page->Scripts) > 0) {
+            foreach ($page->Scripts as $key => $script) {
+                if (strlen($page->SiteVars["Scripts"]) > 0) {
+                    $page->SiteVars["Scripts"] .= "\n";
                 }
-                $Page->SiteVars["Scripts"] .= "<script src=\"".$ScriptDir.$script."\" type=\"text/javascript\"></script>";
+                $page->SiteVars["Scripts"] .= "<script src=\"".$ScriptDir.$script."\" type=\"text/javascript\"></script>";
             }
         }
         //Set the site title
-        if (isset($Page->Title) === true && strlen($Page->Title) > 0) {
-            $Page->SiteVars["SiteTitle"] = $Page->Title;
+        if (isset($page->Title) === true && strlen($page->Title) > 0) {
+            $page->SiteVars["SiteTitle"] = $page->Title;
         }
         //Load the menus
         if (isset($Menus) === true && count($Menus) > 0) {
             foreach ($Menus as $menuId => $menu) {
-                $menuTemplate = $Page->GetSiteSection($menuId);
+                $menuTemplate = $page->GetSiteSection($menuId);
                 $menuItems = "";
                 foreach ($menu->MenuItems as $key => $menuItem) {
-                    if (isset($Pages[$menuItem->PageId])) {
-                        $pageInfo = $Pages[$menuItem->PageId];
+                    if (isset($pages[$menuItem->PageId])) {
+                        $pageInfo = $pages[$menuItem->PageId];
                         $itemAr = array("Link" => $pageInfo->Link, "Name" => $pageInfo->Name, "Class" => $menu->DefaultClass);
-                        if ($menuItem->PageId == $PageId) {
+                        if ($menuItem->PageId == $pageId) {
                             $itemAr["Class"] = $menu->ActiveClass;
                         }
-                        if ((count($menuItem->AltPageIds) > 0) && in_array($PageId, $menuItem->AltPageIds)) {
+                        if ((count($menuItem->AltPageIds) > 0) && in_array($pageId, $menuItem->AltPageIds)) {
                             $itemAr["Class"] = $menu->ActiveClass;
                         }
                         $menuItems .= ArrayReplace($itemAr, $menuTemplate);
                     }
                 }
-                $Page->SetSiteSection($menuId, $menuItems);
+                $page->SetSiteSection($menuId, $menuItems);
             }
         }
         $page->Show();
