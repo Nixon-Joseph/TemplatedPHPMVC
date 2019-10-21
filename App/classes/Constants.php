08dde21625da40afc384a230ce043f4d3ecba010
Joseph Nieminen Nixon <nieminen432@gmail.com>
2019-10-21 13:42:59 -0500

    fixed bad link

--- App/classes/Constants.php
+++ App/classes/Constants.php
@@ -3,6 +3,6 @@ class Constants {
     const SITE_NAME = "SITE_NAME";
     const SITE_SUBTITLE = "SITE_SUBTITLE";
     const SITE_DESCRIPTION = "SITE_DESCRIPTION";
-    const SITE_ADDRESS = "http://localhost/mvc";
+    const SITE_ADDRESS = "http://mvc";
 }
 ?>
