3c8d35ae35f72bee3ee53530d440d5850d5fd339
Joseph Nixon <jnixon@brossgroup.com>
2021-10-19 19:30:17 -0400

    Added page count to getAllPages method

--- src/abstract/Repo.php
+++ src/abstract/Repo.php
@@ -181,7 +181,7 @@ abstract class Repo {
             $sql .= " LIMIT $offset, $pageSize";
             $results = $this->_query($sql);
             $totalRecords = $this->_getCount();
-            return array("results" => $results, "totalRecords" => $totalRecords);
+            return array("results" => $results, "totalRecords" => $totalRecords, "pages" => $totalRecords / $pageSize);
         } catch (\Throwable $th) {
             return null;
         }
